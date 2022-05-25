58e4383e6486c1a5a657b33f73c5401827f830e6
ziyovuddin <ziyovuddinabdullayev487@gmail.com>
2022-05-25 19:14:09 +0500

    Coffe

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let search = document.querySelector(".search");
+document.querySelector("#searchBtn").onclick = function(){
+    search.classList.toggle("active");
+    
+}
+
+let foo = document.querySelector(".foo");
+document.querySelector("#searchBtn2").onclick = function(){
+    foo.classList.toggle("active");
+    
+}
